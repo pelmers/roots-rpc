97de79302944f08e63c736bb288a500b35c952b0
Peter Elmers <peter.elmers@yahoo.com>
2021-05-17 05:32:32 +0000

    Add optional key to socket transport type

--- src/rpcSocketTransport.ts
+++ src/rpcSocketTransport.ts
@@ -2,14 +2,23 @@ import io from "socket.io";
 import { Message, RpcTransport } from "./rpcTypes";
 
 export class SocketTransport implements RpcTransport {
-  constructor(private socket: io.Socket | SocketIOClient.Socket) {}
+  /**
+   * Implements an RPC transport wrapper for socket.io library.
+   * @param socket socket to send/receive data over
+   * @param key optional, add key if multiplexing connections on one socket
+   */
+  constructor(
+    private socket: io.Socket | SocketIOClient.Socket,
+    private key: string = ""
+  ) {}
+
   send(msg: Message) {
-    this.socket.emit("roots-rpc", msg);
+    this.socket.emit(`roots-rpc-${this.key}`, msg);
   }
   onMessage(cb: (msg: Message) => void) {
-    this.socket.on("roots-rpc", cb);
+    this.socket.on(`roots-rpc-${this.key}`, cb);
     return {
-      dispose: () => this.socket.off("roots-rpc", cb),
+      dispose: () => this.socket.off(`roots-rpc-${this.key}`, cb),
     };
   }
 }
